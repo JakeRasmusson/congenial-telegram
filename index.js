6162f101d91cedae9e8f5021976fe171fba1fd04
Jake Rasmusson <jakerasmusson@gmail.com>
2024-04-25 18:39:36 -0500

    Fixes parse functions

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ import * as fs from 'fs';
 import { parseData } from "./parseData.js"
 
 
-const readFiles = async (pathToFiles) => {
+const readFiles = async (pathToFiles, team) => {
     try {
         const data = await fs.readFileSync(pathToFiles, 'utf-8')
-        parseData(data)
+        parseData(data, team)
     } catch (err) {
         console.log(err)
     }
 }
 
-readFiles('./csv/Elkhorn Area Football  Varsity Roster.csv')
+readFiles('./csv/Elkhorn Area Football  Varsity Roster.csv', 'home')
+readFiles('./csv/Westosha Central High School Football  Varsity Roster.csv', 'away')
--- parseData.js
+++ parseData.js
@@ -1,36 +1,33 @@
-const parseData = (data) => {
-    const columnSplit = data.split('\n')
-    const rowSplit = []
+const parseData = (data, team) => {
+    const rowSplit = data.split('\n')
+    const columnSplit = []
     const players = []
-    columnSplit.forEach(column => {
-        let noQuote = column.replaceAll('"', '')
-        rowSplit.push(noQuote.split(','))
-    });
     rowSplit.forEach(row => {
-         let playerNumber = parseInt(row[0]) || 0
+        let noQuote = row.replaceAll('"', '')
+        columnSplit.push(noQuote.split(','))
+    });
+    columnSplit.forEach(column => {
+         let playerNumber = parseInt(column[0])
          console.log(playerNumber)
-        if (playerNumber != 0) {
-            players.push(row)
+        if (!isNaN(playerNumber)) {
+            players.push(column)
         } else {
             console.log('not a player')
         }
     } )
-    prepareForObject(players)
+    prepareForObject(players, team)
 }
 
-const prepareForObject = (players) => {
-    const objectReady = []
-    players.forEach(player => {
-        objectReady.push(player.slice(0,4))
-    })
-    createObjects(objectReady)
+const prepareForObject = (players, team) => {
+    const slicedPlayers = players.map(player => player.slice(0,4))
+    createObjects(slicedPlayers, team)
 }
 
-const createObjects = (objectReady) => {
+const createObjects = (objectReady, team) => {
     const playerObjects = {}
     objectReady.forEach(player => {
         const [ number, name, position, grade] = player
-        playerObjects[number] = {
+        playerObjects[`${number} ${team}`] = {
                 name,
                 position,
                 grade
